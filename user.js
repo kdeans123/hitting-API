f249cf3d9bef880532c9faf4b44cfd82e033233f
Kat <kat.m.deans@gmail.com>
2025-01-01 14:00:12 -0500

    Posts fetching

--- user.js
+++ user.js
@@ -1,5 +1,22 @@
 const postListEl = document.querySelector('.post-list');
 
+async function onSearchChange(event) {
+    const id = event.target.value;
+    const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json();
+    postListEl.innerHTML = postsData.map (post => 
+        `<div class="post">
+            <div class="post__title">
+            ${post.title}
+            </div>
+            <p class="post__body">
+            ${post.body}
+            </p>
+        </div>`
+    ).join('');
+
+}
+
 
 async function main() {
     const id = localStorage.getItem("id")
@@ -8,11 +25,11 @@ async function main() {
 
     postListEl.innerHTML = postsData.map (post => 
         `<div class="post">
-            <div class="${post.title}">
-                Post Title
+            <div class="post__title">
+            ${post.title}
             </div>
-            <p class="${post.body}">
-                Post Body
+            <p class="post__body">
+            ${post.body}
             </p>
         </div>`
     ).join('');
