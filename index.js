432c7b0c7416a0bee63f077aa2a473d9afbe5695
Kat <kat.m.deans@gmail.com>
2025-01-01 12:47:58 -0500

    route to a new page

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-
+const userListEl = document.querySelector('.user-list');
 
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
     const usersData = await users.json();
-    const userListEl = document.querySelector('.user_list');
-
-    userListEl.innerHTML = usersData
-    .map(user => `<div class="user-card">
-            <div class="user-card__container">
-              <h3>User's Name</h4>
-                <p><b>Email:</b> email@email.com</p>
-                <p><b>Phone:</b> 0000000000</p>
-                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-            </div>
-          </div>`)
-          .join("")
+    userListEl.innerHTML = usersData.map(user => userHTML(user)).join("")
 }
 
 main();
+
+function showUserPost(user) {
+    window.location.href = `${window.location.origin}/user.html`
+}
+
+// how to route to a new page in vanilla java script window.location.href
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPost(user)">
+            <div class="user-card__container">
+                <h3>${user.name}</h4>
+                    <p><b>Email:</b> ${user.email}</p>
+                    <p><b>Phone:</b> ${user.phone}</p>
+                    <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+                </div>
+            </div>`;
+}
